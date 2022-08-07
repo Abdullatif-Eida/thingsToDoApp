f15636da903cca3422c4f60c94c30ccbaf0518ed
Abdullatif Eida <mhdabdullatif2016@gmail.com>
2022-08-07 14:49:14 +0300

    fix

---
+++ lib/controllers/users_controller.dart
@@ -0,0 +1,533 @@
+List users = [
+  {
+    "name": "Lamar Shepard",
+    "phone": "(377) 911-6787",
+    "email": "leo.vivamus@icloud.edu",
+    "address": "5768 Eu Avenue",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=2",
+    "lastOnline": "1 sec"
+  },
+  {
+    "name": "Castor Sims",
+    "phone": "1-816-113-5719",
+    "email": "dui@icloud.org",
+    "address": "1514 Sollicitudin Road",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=1",
+    "lastOnline": "1 min"
+  },
+  {
+    "name": "Caleb Evans",
+    "phone": "(521) 548-1806",
+    "email": "cum.sociis@outlook.org",
+    "address": "808-7502 Nulla Rd.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=8",
+    "lastOnline": "6 min"
+  },
+  {
+    "name": "Kuame Stout",
+    "phone": "(712) 123-8752",
+    "email": "scelerisque.neque@outlook.org",
+    "address": "Ap #246-6468 Ante Street",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=1",
+    "lastOnline": "5 min"
+  },
+  {
+    "name": "Ryan Whitehead",
+    "phone": "1-246-226-5583",
+    "email": "sed@hotmail.edu",
+    "address": "6846 Arcu Rd.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=6",
+    "lastOnline": "1 month"
+  },
+  {
+    "name": "Molly Watkins",
+    "phone": "(458) 970-4033",
+    "email": "vitae.posuere.at@outlook.ca",
+    "address": "502-6647 Placerat, Avenue",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=4",
+    "lastOnline": "1 min"
+  },
+  {
+    "name": "Bruce Ballard",
+    "phone": "(848) 978-7264",
+    "email": "dolor@hotmail.ca",
+    "address": "898-109 Suscipit, Ave",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=4",
+    "lastOnline": "5 min"
+  },
+  {
+    "name": "Justine Velazquez",
+    "phone": "(673) 352-0153",
+    "email": "integer@outlook.net",
+    "address": "Ap #943-5730 Lorem Ave",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=3",
+    "lastOnline": "59 min"
+  },
+  {
+    "name": "Arthur Cummings",
+    "phone": "1-228-566-8623",
+    "email": "ligula.eu@protonmail.net",
+    "address": "Ap #897-3832 Nullam Rd.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=5",
+    "lastOnline": "6 min"
+  },
+  {
+    "name": "Ramona Murphy",
+    "phone": "1-564-838-8774",
+    "email": "integer.vitae@icloud.net",
+    "address": "568-7726 Nibh Avenue",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=2",
+    "lastOnline": "5 min"
+  },
+  {
+    "name": "Simon Roberts",
+    "phone": "(581) 711-8686",
+    "email": "imperdiet@icloud.couk",
+    "address": "Ap #449-4427 Sodales. Ave",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=5",
+    "lastOnline": "5 min"
+  },
+  {
+    "name": "Eagan Hahn",
+    "phone": "(615) 276-9487",
+    "email": "nibh.dolor.nonummy@outlook.ca",
+    "address": "Ap #770-6730 Sit St.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=1",
+    "lastOnline": "5 min"
+  },
+  {
+    "name": "Orla Berger",
+    "phone": "(418) 771-4738",
+    "email": "ipsum.porta@outlook.org",
+    "address": "149-6734 Tincidunt Street",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=9",
+    "lastOnline": "5 min"
+  },
+  {
+    "name": "Xanthus Rosario",
+    "phone": "1-743-253-1875",
+    "email": "consectetuer@hotmail.ca",
+    "address": "773 Vel Street",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=5",
+    "lastOnline": "6 min"
+  },
+  {
+    "name": "Brooke Allison",
+    "phone": "1-689-594-2268",
+    "email": "egestas.sed@outlook.com",
+    "address": "943-9141 Aliquam Street",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=7",
+    "lastOnline": "1 month"
+  },
+  {
+    "name": "Cruz Spence",
+    "phone": "(674) 405-7672",
+    "email": "tincidunt@protonmail.org",
+    "address": "2910 Semper Road",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=7",
+    "lastOnline": "1 min"
+  },
+  {
+    "name": "Kelly Galloway",
+    "phone": "1-638-278-0545",
+    "email": "mauris.elit@hotmail.org",
+    "address": "815-1692 Mauris Avenue",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=5",
+    "lastOnline": "15 min"
+  },
+  {
+    "name": "Burton Delacruz",
+    "phone": "(649) 701-1642",
+    "email": "ipsum.ac@yahoo.com",
+    "address": "Ap #517-8767 Diam Street",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=2",
+    "lastOnline": "59 min"
+  },
+  {
+    "name": "Dexter Morin",
+    "phone": "(429) 553-4214",
+    "email": "aliquet.sem@google.net",
+    "address": "214-698 Pede. St.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=5",
+    "lastOnline": "59 min"
+  },
+  {
+    "name": "Howard Golden",
+    "phone": "1-865-833-8681",
+    "email": "justo@google.ca",
+    "address": "569-6480 Donec St.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=8",
+    "lastOnline": "1 sec"
+  },
+  {
+    "name": "Melyssa Allison",
+    "phone": "1-547-337-8236",
+    "email": "erat.volutpat.nulla@google.org",
+    "address": "Ap #848-6111 Vestibulum Rd.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=4",
+    "lastOnline": "1 month"
+  },
+  {
+    "name": "Mary Shaw",
+    "phone": "(694) 739-8549",
+    "email": "amet.ante.vivamus@yahoo.couk",
+    "address": "P.O. Box 272, 5421 Fringilla, Avenue",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=2",
+    "lastOnline": "6 min"
+  },
+  {
+    "name": "Ina May",
+    "phone": "1-393-422-7154",
+    "email": "ornare.facilisis@google.net",
+    "address": "Ap #153-5335 Nunc St.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=8",
+    "lastOnline": "59 min"
+  },
+  {
+    "name": "Francesca Pena",
+    "phone": "1-783-873-3110",
+    "email": "ligula.nullam.enim@aol.net",
+    "address": "914-4414 Ac, St.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=3",
+    "lastOnline": "1 min"
+  },
+  {
+    "name": "Lysandra Cross",
+    "phone": "1-838-802-5145",
+    "email": "quam.a@outlook.net",
+    "address": "9091 Non Avenue",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=3",
+    "lastOnline": "1 sec"
+  },
+  {
+    "name": "Glenna Cervantes",
+    "phone": "(566) 460-5152",
+    "email": "vitae.aliquam@protonmail.ca",
+    "address": "554-6857 In Rd.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=1",
+    "lastOnline": "3 month"
+  },
+  {
+    "name": "Ferris Perry",
+    "phone": "(355) 524-6196",
+    "email": "interdum.libero@aol.org",
+    "address": "192-9629 Mauris Road",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=3",
+    "lastOnline": "59 min"
+  },
+  {
+    "name": "Noah Terrell",
+    "phone": "1-261-566-8614",
+    "email": "malesuada@protonmail.org",
+    "address": "P.O. Box 164, 6193 Eget, Road",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=8",
+    "lastOnline": "6 min"
+  },
+  {
+    "name": "Kato Ayala",
+    "phone": "1-343-168-5543",
+    "email": "magna.nam.ligula@icloud.ca",
+    "address": "1069 Facilisis Rd.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=8",
+    "lastOnline": "15 min"
+  },
+  {
+    "name": "Rooney Dillard",
+    "phone": "(651) 824-0661",
+    "email": "vivamus.nibh@protonmail.com",
+    "address": "Ap #541-1208 Sed Avenue",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=1",
+    "lastOnline": "6 min"
+  },
+  {
+    "name": "Barry Ortega",
+    "phone": "(378) 848-5487",
+    "email": "mauris.blandit@outlook.couk",
+    "address": "3646 Egestas St.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=2",
+    "lastOnline": "15 min"
+  },
+  {
+    "name": "Phelan Higgins",
+    "phone": "(434) 714-7305",
+    "email": "suspendisse@google.org",
+    "address": "Ap #952-2781 Mauris Av.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=3",
+    "lastOnline": "1 min"
+  },
+  {
+    "name": "Hashim Ramirez",
+    "phone": "(428) 740-2282",
+    "email": "blandit.mattis@google.net",
+    "address": "129-1305 Tincidunt Rd.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=9",
+    "lastOnline": "3 month"
+  },
+  {
+    "name": "Libby Emerson",
+    "phone": "1-238-462-3397",
+    "email": "velit.sed@outlook.ca",
+    "address": "973-3103 Suspendisse Rd.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=6",
+    "lastOnline": "5 min"
+  },
+  {
+    "name": "Bethany Harvey",
+    "phone": "(916) 250-4627",
+    "email": "egestas.aliquam@yahoo.org",
+    "address": "Ap #210-1235 Vestibulum Street",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=6",
+    "lastOnline": "1 sec"
+  },
+  {
+    "name": "Jamal Savage",
+    "phone": "(579) 562-5485",
+    "email": "facilisis@outlook.net",
+    "address": "8991 Magna St.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=2",
+    "lastOnline": "5 min"
+  },
+  {
+    "name": "Troy Sullivan",
+    "phone": "1-851-733-6370",
+    "email": "fames.ac@protonmail.com",
+    "address": "471-6723 Vestibulum Avenue",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=8",
+    "lastOnline": "6 min"
+  },
+  {
+    "name": "Neve Alston",
+    "phone": "(577) 542-2690",
+    "email": "donec.felis.orci@google.ca",
+    "address": "Ap #774-6885 Nibh Road",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=6",
+    "lastOnline": "1 month"
+  },
+  {
+    "name": "Fletcher Allen",
+    "phone": "1-227-581-7344",
+    "email": "enim.non@protonmail.ca",
+    "address": "P.O. Box 876, 8343 Amet Street",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=2",
+    "lastOnline": "1 min"
+  },
+  {
+    "name": "Nissim Clark",
+    "phone": "1-180-230-3837",
+    "email": "nec.tempus@outlook.edu",
+    "address": "768-4849 Donec Avenue",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=3",
+    "lastOnline": "15 min"
+  },
+  {
+    "name": "Reese Daniels",
+    "phone": "(959) 345-6561",
+    "email": "leo@aol.edu",
+    "address": "8263 Vestibulum, Street",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=4",
+    "lastOnline": "1 month"
+  },
+  {
+    "name": "Erasmus Carrillo",
+    "phone": "1-876-475-4853",
+    "email": "nunc.sollicitudin@google.ca",
+    "address": "Ap #552-690 Phasellus Avenue",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=2",
+    "lastOnline": "6 min"
+  },
+  {
+    "name": "Lionel Tyler",
+    "phone": "1-475-237-3831",
+    "email": "et@hotmail.edu",
+    "address": "1320 Nunc Street",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=1",
+    "lastOnline": "5 min"
+  },
+  {
+    "name": "Wade Johns",
+    "phone": "1-535-627-9339",
+    "email": "nulla.facilisi@outlook.ca",
+    "address": "994-1762 A St.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=7",
+    "lastOnline": "1 sec"
+  },
+  {
+    "name": "Sebastian Watts",
+    "phone": "(265) 577-5648",
+    "email": "non.cursus@aol.couk",
+    "address": "390-9415 Sed Av.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=7",
+    "lastOnline": "1 sec"
+  },
+  {
+    "name": "Kelsie Bowen",
+    "phone": "1-253-792-0131",
+    "email": "enim.non@aol.net",
+    "address": "5474 Id St.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=7",
+    "lastOnline": "1 min"
+  },
+  {
+    "name": "Connor Kirkland",
+    "phone": "1-576-461-2815",
+    "email": "tempor.bibendum@outlook.edu",
+    "address": "P.O. Box 882, 6947 Scelerisque Avenue",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=4",
+    "lastOnline": "5 min"
+  },
+  {
+    "name": "James Combs",
+    "phone": "(468) 271-7885",
+    "email": "nunc.sed@aol.com",
+    "address": "3889 Mollis Road",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=3",
+    "lastOnline": "1 min"
+  },
+  {
+    "name": "Devin Pearson",
+    "phone": "(889) 360-0475",
+    "email": "sed.congue@hotmail.edu",
+    "address": "Ap #744-8539 Cras Av.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=8",
+    "lastOnline": "6 min"
+  },
+  {
+    "name": "Germane Winters",
+    "phone": "(325) 437-6312",
+    "email": "curae.phasellus.ornare@icloud.ca",
+    "address": "Ap #436-9435 Dictum St.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=2",
+    "lastOnline": "1 sec"
+  },
+  {
+    "name": "Alfonso Combs",
+    "phone": "1-256-667-3220",
+    "email": "convallis.convallis@yahoo.org",
+    "address": "P.O. Box 481, 6263 Quisque Rd.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=4",
+    "lastOnline": "3 month"
+  },
+  {
+    "name": "Grant Tran",
+    "phone": "(677) 333-5788",
+    "email": "dictum.cursus.nunc@google.org",
+    "address": "Ap #499-9869 Lorem Avenue",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=6",
+    "lastOnline": "1 sec"
+  },
+  {
+    "name": "Finn Jenkins",
+    "phone": "(575) 133-2241",
+    "email": "mauris.aliquam@aol.net",
+    "address": "Ap #819-3308 Semper Ave",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=9",
+    "lastOnline": "1 sec"
+  },
+  {
+    "name": "Emma Lindsey",
+    "phone": "1-687-435-1317",
+    "email": "et.rutrum.eu@yahoo.edu",
+    "address": "802-4742 Vel St.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=7",
+    "lastOnline": "3 month"
+  },
+  {
+    "name": "Brady Black",
+    "phone": "1-450-557-3546",
+    "email": "interdum.curabitur@aol.edu",
+    "address": "972-3286 Cras Street",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=1",
+    "lastOnline": "15 min"
+  },
+  {
+    "name": "Ethan Hobbs",
+    "phone": "1-566-166-4546",
+    "email": "cras.convallis.convallis@aol.ca",
+    "address": "4470 Mollis Road",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=1",
+    "lastOnline": "1 month"
+  },
+  {
+    "name": "Quentin Gates",
+    "phone": "1-455-271-7626",
+    "email": "aenean.massa@google.couk",
+    "address": "694 Cum Avenue",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=4",
+    "lastOnline": "3 month"
+  },
+  {
+    "name": "Slade Compton",
+    "phone": "(218) 722-6874",
+    "email": "vivamus.non@icloud.com",
+    "address": "3525 Ipsum St.",
+    "isAcepted": "True",
+    "url": "https://source.unsplash.com/random//200x200?sig=9",
+    "lastOnline": "3 month"
+  },
+  {
+    "name": "Bertha Hebert",
+    "phone": "1-367-686-9765",
+    "email": "suspendisse.tristique.neque@aol.ca",
+    "address": "P.O. Box 288, 9904 Sed, Av.",
+    "isAcepted": "False",
+    "url": "https://source.unsplash.com/random//200x200?sig=3",
+    "lastOnline": "6 min"
+  }
+];
